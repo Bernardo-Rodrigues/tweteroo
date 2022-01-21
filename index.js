80f01194254ad6965dfa0ec7ea4748e136bcbb79
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-01-21 13:53:59 -0300

    feat: add sign-up

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const server = express()
 server.use(cors())
 server.use(json())
 server.listen(5000)
+
+const users = []
+const tweets = []
+
+server.post("/sign-up", (req, res) => {
+    users.push(req.body)
+    res.send("OK")
+})
+
